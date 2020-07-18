bcfb95d341ada7c7afb0940e5902405148fba2b0
xEntrophy <61253468+xEntrophy@users.noreply.github.com>
2020-07-18 14:56:13 -0500

    sipra normalizado

--- Aptitudes_cultivos/nombrar_archivos.py
+++ Aptitudes_cultivos/nombrar_archivos.py
@@ -42,40 +42,6 @@ for cultivo in df_ini['Cultivo']:
 
 df_ini['Cultivo']=cultivos
 
-# Organizar nombres de departamentos
-df_norm = df_ini
-
-# Coger los nombres de departamentos del Json
-import json
-with open(path+'departamentos.json') as f:
-    counties = json.load(f)
-
-Departamentos = []
-geocol = counties.copy()
-#Sacar una lista de los departamentos llamada "Departamentos"
-for man in geocol['features']:
-    Departamentos.append(man['properties']['NOMBRE_DPT'])
-    man['id']=man['properties']['NOMBRE_DPT']
-
-# Por cada departamento del Json
-for depto in Departamentos:
-    # si está ya en el data set, no hacer nada, si no está hacer lo siguiente
-    if df_norm['Departamento'].str.contains(depto).any() == False:
-        # Revisar cantidad de palabras
-        words = depto.split()
-        # Si hay una sola palabra, coger el 60 % de esa palabra y buscarlo
-        if len(words) == 1:
-            df_norm.loc[df_norm.Departamento.str.find(depto[:int(len(depto)*0.6)])>=0, 'Departamento'] = depto
-        # Si hay entre 2 o 3 palabras por lo general se encuentra casi la misma palabra (80%)
-        elif len(words) > 1 and len(words) <= 3:
-            df_norm.loc[df_norm.Departamento.str.find(depto[:int(len(depto)*0.8)])>=0, 'Departamento'] = depto
-        # si hay mas de 4 palabras, el unico registro es san andres
-        elif len(words) > 4:
-            df_norm.loc[df_norm.Departamento.str.find("SAN ANDRES")>=0, 'Departamento'] = depto
-
 # Export dataframe as csv
 df_ini.to_csv("C:/Users/guill/Documents/GitHub/project/Aptitudes_cultivos/data_sipra.csv")
 df_ini.to_excel("C:/Users/guill/Documents/GitHub/project/Aptitudes_cultivos/data_sipra.xlsx")
-
-df_norm.to_csv("C:/Users/guill/Documents/GitHub/project/Aptitudes_cultivos/data_sipra_normalizado.csv")
-df_norm.to_excel("C:/Users/guill/Documents/GitHub/project/Aptitudes_cultivos/data_sipra_normalizado.xlsx")
